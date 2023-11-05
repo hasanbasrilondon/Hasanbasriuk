4dfed985685d896e74ceb79cd4b71d481044f3d6
hasanbasrilondon <142103579+hasanbasrilondon@users.noreply.github.com>
2023-11-05 17:48:14 +0000

    Version2

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -2,6 +2,8 @@ public class Class1 {
     public static void main(String[] args) {
         // Version1
         System.out.println("Version1");
+        System.out.println("Version2");
+
 
     }
 
